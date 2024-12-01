b85066ba854bc5a244cf3b9472844bd671b8c8e2
dipgit.com <diprajdhar08@gmail.com>
2024-12-01 12:07:24 +0600

    that is the engine part of this car

---
+++ comdelta/vehicle/CarEngine.java
@@ -0,0 +1,11 @@
+package design_pattern_application_Dipraj.comdelta.vehicle;
+
+
+
+public class CarEngine implements Engine {
+    @Override
+    public void createEngine() {
+        System.out.println("Creating car engine...");
+    }
+}
+

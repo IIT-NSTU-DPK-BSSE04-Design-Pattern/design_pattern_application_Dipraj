eae4426f2c2ec8fd1c663c14746278a3741d353e
dipgit.com <diprajdhar08@gmail.com>
2024-12-01 12:07:00 +0600

    Body part of this car

---
+++ comdelta/vehicle/CarBody.java
@@ -0,0 +1,10 @@
+package design_pattern_application_Dipraj.comdelta.vehicle;
+
+
+public class CarBody implements Body {
+    @Override
+    public void createBody() {
+        System.out.println("Creating car body...");
+    }
+}
+
